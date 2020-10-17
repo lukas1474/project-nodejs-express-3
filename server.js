b04a6722319c993eaef221be2381a2366a810dd4
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-17 22:38:20 +0200

    init project, add first array with datas, add first endpoints

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const {db} = require('./db');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/testimonials', cors(), (req, res) => {
+    res.json(db);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    res.render('testimonialsID', {id: req.params.id});
+});
+
+app.get('/testimonials/random', (req, res) => {
+    res.render('testimonialsID', {id: req.params.id});
+});
+
+app.listen(8000, () => {
+    console.log('CORS-enabled web server listening on port 80')
+});

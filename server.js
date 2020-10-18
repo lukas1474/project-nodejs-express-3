20e7d55377b3a9d8e0e5dcb5b312aaebca520935
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-18 19:43:50 +0200

    add app.get for testimonials/:id and app. use for 404 error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const {db} = require('./db');
+const { db } = require('./db');
 
 const app = express();
 
@@ -10,15 +10,28 @@ app.use(cors());
 
 app.get('/testimonials', (req, res, next) => {
     res.json(db);
-  })
+})
 
-app.get('/testimonials/:id', (req, res) => {
-    res.render('testimonialsID', {id: req.params.id});
-});
+app.get('/testimonials/:id', (req, res, next) => {
+    const id = req.params.id;
+    if (id === 'testimonial') {
+        id = Math.floor(Math.random() * db.length);
+    } else {
+        const result = db.filter(position => {
+            return position.id == id;
+        });
+
+        res.json(result);
+    }
+})
 
 app.get('/testimonials/random', (req, res) => {
-    res.render('testimonialsID', {id: req.params.id});
+    res.render('testimonialsID', { id: req.params.id });
 });
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+})
 
 app.listen(8000, () => {
     console.log('CORS-enabled web server listening on port 8000')

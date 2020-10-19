b300ce68a1c8ea059bd69b76f36cfdb7f7ce9692
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-19 23:27:07 +0200

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const testimonials = require('./routes/testimonials.routes');
 const concerts = require('./routes/concerts.routes');
 const seats = require('./routes/seats.routes');
+const path = require('path');
 
 const app = express();
 
@@ -10,14 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/', testimonials);
-app.use('/', concerts);
-app.use('/', seats);
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use('/api', testimonials);
+app.use('/api', concerts);
+app.use('/api', seats);
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
-    console.log('CORS-enabled web server listening on port 8000')
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  });

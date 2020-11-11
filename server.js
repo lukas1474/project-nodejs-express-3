25bddfc0f95ecb941e7aac340898fdedbeb8ab25
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-11-11 19:15:52 +0100

    modify server for mongoose and add models

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
+
 const testimonials = require('./routes/testimonials.routes');
 const concerts = require('./routes/concerts.routes');
 const seats = require('./routes/seats.routes');
@@ -30,6 +32,14 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
+
+mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running on port:`, 8000);
